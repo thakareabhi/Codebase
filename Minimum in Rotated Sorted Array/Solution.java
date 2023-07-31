bc3780204d48214d334433b81fcf750330349e22
ABHINAV THAKARE <51523495+thakareabhi@users.noreply.github.com>
2023-07-31 19:57:11 +0530

    Update Solution.java

--- Minimum in Rotated Sorted Array/Solution.java
+++ Minimum in Rotated Sorted Array/Solution.java
@@ -1,17 +1,10 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
-        int sum=0;
-        int arrmax=nums[0];
-
-
-        for(int i=0;i<nums.length;i++){
-            sum=sum+nums[i];
-            arrmax=Integer.max(sum,arrmax);
-            if(sum<0){
-                sum=0;
+    public int findMin(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+              return nums[i+1];
             }
         }
-        return arrmax;
-
+        return nums[0];
     }
 }
